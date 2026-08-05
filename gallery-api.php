e268ea40883c552dfdbf427376880f06c5f29852
anannapro <thegreatdolmen.004@gmail.com>
2026-08-06 02:55:05 +0600

    Update gallery-api.php

--- gallery-api.php
+++ gallery-api.php
@@ -1,40 +1,53 @@
 <?php
 header('Content-Type: application/json');
 
-$baseDir = __DIR__;
-$allowedPhotos = ['jpg', 'jpeg', 'png', 'webp', 'gif'];
-$allowedVideos = ['mp4', 'webm', 'mov', 'mkv'];
+function getMediaFiles($dir, $allowedExtensions) {
+    $media = [];
+    if (!is_dir($dir)) return $media;
 
-$albums = [];
+    // Read items in the root folder (e.g., Photos/ or Videos/)
+    $items = array_diff(scandir($dir), array('.', '..'));
 
-function scanMediaFolder($folderName, $type, $allowedExtensions, $baseDir, &$albums) {
-    $dir = $baseDir . '/' . $folderName;
-    if (!is_dir($dir)) return;
+    foreach ($items as $item) {
+        $path = $dir . '/' . $item;
 
-    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
-    foreach ($iterator as $file) {
-        if ($file->isDir()) continue;
-        
-        $ext = strtolower($file->getExtension());
-        if (in_array($ext, $allowedExtensions)) {
-            // Normalize slashes
-            $relativePath = str_replace('\\', '/', substr($file->getPathname(), strlen($baseDir) + 1));
-            $pathParts = explode('/', $relativePath);
-
-            // Determine album name from subfolder
-            $albumName = (count($pathParts) > 2) ? $pathParts[1] : 'General';
-
-            $albums[$albumName][] = [
-                'type' => $type,
-                'url'  => $relativePath,
-                'name' => pathinfo($file->getFilename(), PATHINFO_FILENAME)
-            ];
+        if (is_dir($path)) {
+            // It's an album subfolder! Scan inside it.
+            $albumName = $item;
+            $files = array_diff(scandir($path), array('.', '..'));
+            
+            foreach ($files as $file) {
+                $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+                if (in_array($ext, $allowedExtensions)) {
+                    $media[] = [
+                        'src' => $path . '/' . $file,
+                        'album' => $albumName,
+                        'name' => pathinfo($file, PATHINFO_FILENAME)
+                    ];
+                }
+            }
+        } else {
+            // It's a direct file (Uncategorized / General)
+            $ext = strtolower(pathinfo($item, PATHINFO_EXTENSION));
+            if (in_array($ext, $allowedExtensions)) {
+                $media[] = [
+                    'src' => $path,
+                    'album' => 'General',
+                    'name' => pathinfo($item, PATHINFO_FILENAME)
+                ];
+            }
         }
     }
+    return $media;
 }
 
-scanMediaFolder('photos', 'photo', $allowedPhotos, $baseDir, $albums);
-scanMediaFolder('videos', 'video', $allowedVideos, $baseDir, $albums);
+$imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'svg'];
+$videoExtensions = ['mp4', 'webm', 'ogg', 'mov', 'mkv'];
 
-echo json_encode($albums);
+$response = [
+    'photos' => getMediaFiles('Photos', $imageExtensions),
+    'videos' => getMediaFiles('Videos', $videoExtensions)
+];
+
+echo json_encode($response);
 ?>
